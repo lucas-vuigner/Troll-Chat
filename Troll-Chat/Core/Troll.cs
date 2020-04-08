dbf7c05c4c97eae431251f86ab5a00b52d352e69
Seavice <61095646+Seavice@users.noreply.github.com>
2020-04-08 20:41:26 +0300

    Add files via upload

---
+++ Troll-Chat/Core/Troll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnsoulSharp;
+using Troll.Event;
+using Troll.Menu;
+
+namespace Troll.Core
+{
+    public class Troll
+    {
+        public static void OnLoad()
+        {
+            MenuManager.LoadMenu();
+            EventManager.LoadEvents();
+        }
+    }
+}
---
+++ Troll-Chat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using Troll.Core;
+
+namespace Troll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += GameEventOnOnGameLoad;
+        }
+
+        private static void GameEventOnOnGameLoad()
+        {
+            Core.Troll.OnLoad();
+        }
+    }
+}
